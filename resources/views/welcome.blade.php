ccf0440a26d55a01fe5a25eeb2d7c1f5e27ed466
yudantaa <yudanta.agasta@gmail.com>
2024-09-03 17:30:45 +0800

    initialization

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Dore Production</title>
+@vite('resources/css/app.css')
+</head>
+<body class="bg-gray-100 text-gray-900">
+<div class="container mx-auto p-4">
+<h1 class="text-4xl font-bold text-center">Dore Production</h1>
+<p class="mt-4 text-lg text-center">Work In Progress    </p>
+</div>
+</body>
+</html>
